6a104f5781d2854a0030d55d7f475bc140bec428
SMaribbay <78175106+SMaribbay@users.noreply.github.com>
2021-11-09 16:32:46 -0500

    Revised after class

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
+
+var button = document.getElementById("button");
+var userInput = document.getElementById("userInput");
+var output = document.getElementById("outputText");
 var fortunes = [
+  "will win free food for the rest of your life",
+  "will receive $1 million.",
+  "will meet your soulmate",
+  "will get nothing",
+  "will reach your goals"
+]
 
-  "You will win free food for the rest of your life",
-  "You will meet your soulmate",
-  "You will receive $1 million",
-  "You will receive absolutely nothing"
-];
-
-function tellFortune() {
+button.addEventListener("click", restyle);
 
 
-  var randomNumber = Math.floor(Math.random() * fortunes.length);
+function fortune(name){
+  output.innerText = name +" " + fortunes[Math.floor(Math.random()*fortunes.length)];
 
-  // print the random number to the console log to make sure it's working
-  console.log("random number: " + randomNumber);
-
-  // take the fortune at the random spot in the array and insert it into the "fortune" div
- document.getElementById("fortune").innerHTML = fortunes[randomNumber];
 }
 
 function restyle() {
-    var r = Math.floor(Math.random() * 100);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 150);
-    var bgColor = "rgb(" + r + "," + g + "," + b + ")";
- console.log(bgColor);
+  var r = Math.random() * 200;
+  var g = Math.random() * 255;
+  var b = Math.random() * 255;
+  var color = "rgb(" + r + "," + g + "," + b + ")";
+  var bcolor = "rgba(" + r + "," + g + "," + b + ", .10 )";
+  output.style.backgroundColor = bcolor;
+  output.style.color = color;
 
-    document.body.style.background = bgColor;
-    }
 
-random_bg_color();
+  fortune (document.getElementById("userInput").value);
+}
